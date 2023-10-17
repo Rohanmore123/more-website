9ef736f46f064ad7c0ddd26b27fa29f38d5ab497
Rohanmore123 <97122575+Rohanmore123@users.noreply.github.com>
2023-10-17 04:38:10 +0000

    added some data

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 
 app=Flask(__name__)
 
 @app.route("/")
-def Hello_world():
-  return "Hello World"
+def Hello_ROHAN():
+  return render_template('home.html')
 
 print(__name__)
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
+
